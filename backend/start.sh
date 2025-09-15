71b657a6996615f94f2c0739a60763ae329bc8a2
mohsen <mohsen.fayyazi@gmail.com>
2025-09-15 19:07:55 -0400

    update backend to deploy on Koyeb

--- backend/start.sh
+++ backend/start.sh
@@ -17,7 +17,13 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='admin@billder.com').exists():
-    User.objects.create_superuser('admin', 'admin@billder.com', 'admin123')
+    User.objects.create_superuser(
+        email='admin@billder.com',
+        password='admin123',
+        first_name='Admin',
+        last_name='User',
+        role='admin'
+    )
     print('Superuser created')
 else:
     print('Superuser already exists')
